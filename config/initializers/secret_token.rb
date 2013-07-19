23351ed07171410bef7fd98e60ca16f8d70e6ded
root <root@domU-12-31-39-0C-DD-FD.compute-1.internal>
2013-07-19 04:24:01 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nasim::Application.config.secret_token = '0045909eda4d1d1a3e4b5dcd32caff0bfdb32b4dee8717026d3143b910b67b7157a0473c8eb19ae9d2026aa84ebb856a0582d8cbb9991655e4d3f84332dd4828'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nasim::Application.config.session_store :cookie_store, key: '_nasim_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nasim::Application.config.session_store :active_record_store
